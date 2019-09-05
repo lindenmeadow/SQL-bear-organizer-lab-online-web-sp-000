ec7339f413c7bc0b9a97782d1a7bfb34bb91f0f9
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2019-09-05 20:08:02 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -5,3 +5,4 @@ INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergea
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 4, "female", "brown", "friendly", true);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 15, "male", "brown", "mean", true);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 3, "female", "black", "loving", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 13, "male", "brown", "evil", false);
