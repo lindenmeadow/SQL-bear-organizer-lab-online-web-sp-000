81ce9d67a4eb9cff0516f61a9cd2f4d28443b5d5
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2019-09-05 20:01:07 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,2 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 8, "male", "brown", "docile", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 5, "male", "black", "gruff", true)
