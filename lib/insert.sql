430e89330b432e9bda3a4ead25277ac39c90529d
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2019-09-05 20:21:58 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 8, "male", "brown", "docile", true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 5, "male", "black", "gruff", true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 9, "female", "black", "happy", true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 12, "male", "white", "surly", true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 4, "female", "brown", "friendly", true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 15, "male", "brown", "mean", true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 3, "female", "black", "loving", true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 13, "male", "brown", "evil", false);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 8, "male", "brown", "docile", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 5, "male", "black", "gruff", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 9, "female", "black", "happy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 12, "male", "white", "surly", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 4, "female", "brown", "friendly", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 15, "male", "brown", "mean", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 3, "female", "black", "loving", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 13, "male", "brown", "evil", 0);
